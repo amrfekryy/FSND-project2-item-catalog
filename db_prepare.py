6d71616ca18b6bf77143a5b220c1d34ea38c8393
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-05-27 11:31:47 +0200

    PEP8 db_prepare.py

--- db_prepare.py
+++ db_prepare.py
@@ -4,20 +4,21 @@ sys.path.append("./database/")
 
 from database import db_setup, db_populate, db_print
 
+
 def main():
 
-	# setup database
-	db_setup.main()
-	print("------------------------")
-	# populate databae with initial data
-	db_populate.main()
-	print("------------------------")
-	# print database contents
-	db_print.main()
-	print("------------------------")
-	
-	print("\nDatabase has been prepared\n")
+    # setup database
+    db_setup.main()
+    print("------------------------")
+    # populate databae with initial data
+    db_populate.main()
+    print("------------------------")
+    # print database contents
+    db_print.main()
+    print("------------------------")
+
+    print("\nDatabase has been prepared\n")
 
 
 if __name__ == '__main__':
-	main()
+    main()
